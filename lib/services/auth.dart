da14a195041ee65e02570f03f0f218f43d2b5f0a
Ravi <ravishankarsingh1996@gmail.com>
2020-05-08 20:14:16 +0530

    Code Review comments added

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,33 +5,32 @@ import '../user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
 // user obj based on firebase user
-  User _userFromFirebaseUser(FirebaseUser user){
-    return user != null ? User(uid: user.uid): null;
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
   }
+
 // auth change user stream
-  Stream<User> get user{
+  Stream<User> get user {
     return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
   }
 
   // register with email and password
-  Future registerWithEmailAndPassword(
-      String email, String password, String empId, String firstName,String lastName,int vendor) async {
+  Future registerWithEmailAndPassword(String email, String password,
+      String empId, String firstName, String lastName, int vendor) async {
     try {
 //      print(email+'@'+password+'@'+empId+'@'+firstName+'@'+lastName+'@');
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
       Firestore.instance.collection('account').document(user.uid).setData({
-
         "email": email,
         "empId": empId,
         "fname": firstName,
         "surname": lastName,
         "uid": user.uid,
         "vendor": vendor,
-
-
       });
       return _userFromFirebaseUser(user);
     } catch (error) {
@@ -39,6 +38,4 @@ class AuthService {
       return null;
     }
   }
-
-
 }
